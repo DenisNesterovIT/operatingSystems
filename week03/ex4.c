5930ef668dd276a8c86570d23d5fc4139927b707
DenisNesterovIT <144430448+DenisNesterovIT@users.noreply.github.com>
2023-09-26 23:54:21 +0300

    Add files via upload

--- week03/ex4.c
+++ week03/ex4.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void* add(const void* a, const void* b) {
-    printf("%lu\n", sizeof(b));
+void* add_int(const void* a, const void* b) {
+    int result = *(int*)a + *(int*)b;
+    return &result;
+}
+
+void* multiply_int(const void* a, const void* b) {
+    int result = *(int*)a * *(int*)b;
+    return &result;
+}
+
+void* maxInt(const void* a, const void* b) {
+    return (*(int*)a > *(int*)b) ? (void*)a : (void*)b;
+}
+
+void* add_double(const void* a, const void* b) {
     double result = *(double*)a + *(double*)b;
     return &result;
 }
 
-void* multiply(const void* a, const void* b) {
+void* multiply_double(const void* a, const void* b) {
     double result = *(double*)a * *(double*)b;
     return &result;
 }
 
-void* max(const void* a, const void* b) {
+void* maxDouble(const void* a, const void* b) {
     return (*(double*)a > *(double*)b) ? (void*)a : (void*)b;
 }
 
@@ -25,27 +38,25 @@ void* aggregate(void* base, size_t size, int n, void* initial_value, void* (*opr
 }
 
 int main() {
-    double double_array[] = {1.0, 2.0, 3.0, 4.0, 5.0};
-    int int_array[] = {1, 2, 3, 4, 5};
-
+    double double_array[] = {2.0, 4.0, 6.0, 8.0, 10.0};
+    int int_array[] = {2, 4, 6, 8, 10};
     double initial_double_add = 0.0;
-    double* sum_double = (double*)aggregate(double_array, sizeof(double), 5, &initial_double, add);
-    printf("Sum of double_array: %lf\n", *sum_double);
+    double* sum_double = (double*)aggregate(double_array, sizeof(double), 5, &initial_double_add, add_double);
+    printf("Sum of double: %lf\n", *sum_double);
     double initial_double_mult = 1.0;
-    double* product_double = (double*)aggregate(double_array, sizeof(double), 5, &initial_double_mult, multiply);
-    printf("Product of double_array: %lf\n", *product_double);
+    double* product_double = (double*)aggregate(double_array, sizeof(double), 5, &initial_double_mult, multiply_double);
+    printf("Product of double: %lf\n", *product_double);
     double initial_double_max = double_array[0];
-    double* max_double = (double*)aggregate(double_array, sizeof(double), 5, &initial_double_max, max);
-    printf("Max of double_array: %lf\n", *max_double);
+    double* max_double = (double*)aggregate(double_array, sizeof(double), 5, &initial_double_max, maxDouble);
+    printf("Max of double: %lf\n", *max_double);
     int initial_int_add = 0;
-    int* sum_int = (int*)aggregate(int_array, sizeof(int), 5, &initial_int_add, add);
-    printf("Sum of int_array: %d\n", *sum_int);
+    int* sum_int = (int*)aggregate(int_array, sizeof(int), 5, &initial_int_add, add_int);
+    printf("Sum of int: %d\n", *sum_int);
     int initial_int_mult = 1;
-    int* product_int = (int*)aggregate(int_array, sizeof(int), 5, &initial_int_mult, multiply);
-    printf("Product of int_array: %d\n", *product_int);
+    int* product_int = (int*)aggregate(int_array, sizeof(int), 5, &initial_int_mult, multiply_int);
+    printf("Product of int: %d\n", *product_int);
     int initial_int_max = int_array[0];
-    int* max_int = (int*)aggregate(int_array, sizeof(int), 5, &initial_int_max, max);
-    printf("Max of int_array: %d\n", *max_int);
-
+    int* max_int = (int*)aggregate(int_array, sizeof(int), 5, &initial_int_max, maxInt);
+    printf("Max of int: %d\n", *max_int);
     return 0;
 }
